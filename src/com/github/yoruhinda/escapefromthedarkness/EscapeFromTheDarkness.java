c4f4d27457237c4cf3ff27790d0d119588d57f44
Ruan Phablo <ruanphablo778@gmail.com>
2023-10-09 18:53:33 -0300

    Feat: Create Gameloop FPS UPS

--- src/com/github/yoruhinda/escapefromthedarkness/EscapeFromTheDarkness.java
+++ src/com/github/yoruhinda/escapefromthedarkness/EscapeFromTheDarkness.java
@@ -1,7 +1,9 @@
 package com.github.yoruhinda.escapefromthedarkness;
 
+import com.github.yoruhinda.escapefromthedarkness.game.Game;
+
 public class EscapeFromTheDarkness {
     public static void main(String[] args) {
-
+        new Game();
     }
 }
--- src/com/github/yoruhinda/escapefromthedarkness/game/Game.java
+++ src/com/github/yoruhinda/escapefromthedarkness/game/Game.java
@@ -1,4 +1,81 @@
 package com.github.yoruhinda.escapefromthedarkness.game;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Game extends Thread{
+    private boolean running = true;
+    private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel();
+
+    public Game() {
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Escape From The Darkness");
+        panel.setBackground(Color.RED);
+        frame.add(panel);
+        frame.setVisible(true);
+        start();
+    }
+
+    @Override
+    public void run(){
+        final int desiredFPS = 60;
+        final int desiredUPS = 60;
+
+        final long updateThreshold = 1000000000 / desiredUPS;
+        final long drawThreshold = 1000000000 / desiredFPS;
+
+        long lastFPS = 0, lastUPS = 0, lastFPSUPSOutput = 0;
+
+        int fps = 0, ups = 0;
+
+        loop:
+        while(running){
+            if((System.nanoTime() - lastFPSUPSOutput) > 1000000000){
+                System.out.println("FPS: " + (double)fps);
+                System.out.println("UPS: " + (double)ups);
+
+                fps = 0;
+                ups = 0;
+
+                lastFPSUPSOutput = System.nanoTime();
+            }
+            if((System.nanoTime() - lastUPS) > updateThreshold){
+                lastUPS = System.nanoTime();
+                update();
+                ups++;
+            }
+            if((System.nanoTime() - lastFPS) > drawThreshold){
+                lastFPS = System.nanoTime();
+                render();
+                fps++;
+            }
+            if(!((System.nanoTime() - lastUPS) > updateThreshold || (System.nanoTime() - lastFPS) > drawThreshold)){
+                long nextScheduledUP = lastUPS + updateThreshold;
+                long nextScheduledDraw = lastFPS + drawThreshold;
+
+                long minScheduled = Math.min(nextScheduledUP, nextScheduledDraw);
+
+                long nanosToWait = minScheduled - System.nanoTime();
+
+                if(nanosToWait <= 0)
+                    continue loop;
+
+                try{
+                    Thread.sleep(nanosToWait / 1000000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void update(){
+
+    }
+
+    private void render(){
+
+    }
 }
